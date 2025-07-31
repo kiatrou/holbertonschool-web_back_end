ee86767c287c71bdbd0639ef6da913a473c8a2c8
Kassandra <kassandraiatrou@gmail.com>
2025-07-31 22:04:31 +1000

    task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     # Create n tasks (coroutines) but don't run them yet
     tasks = [task_wait_random(max_delay) for _ in range(n)]
 
