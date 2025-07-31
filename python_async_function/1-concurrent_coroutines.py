a9617ee88a4114e21f6abef59bd15e42c68b59f7
Kassandra <kassandraiatrou@gmail.com>
2025-07-31 11:24:00 +1000

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -27,6 +27,15 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     # Insert each result in ascending order
     ordered_delays = []
     for delay in results:
-        bisect.insort(ordered_delays, delay)
+        # Find the right position to insert
+        inserted = False
+        for i in range(len(ordered_delays)):
+            if delay < ordered_delays[i]:
+                ordered_delays.insert(i, delay)
+                inserted = True
+                break
+        # If not inserted, it belongs at the end
+        if not inserted:
+            ordered_delays.append(delay)
 
     return ordered_delays
