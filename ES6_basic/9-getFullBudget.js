c689ed16fd2ef9029f8633469bcdb5fd83f2beee
Kassandra <kassandraiatrou@gmail.com>
2025-08-26 11:15:24 +1000

    task 9

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,18 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
+
+// Using a shorthand syntax for defining methods (functions) inside objects. Instead of writing methodName: function() {}, you just write methodName() {}
