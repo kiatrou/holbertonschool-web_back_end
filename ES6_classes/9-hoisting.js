faf5eb6344a9749fd964d683fc6a6776ee045fa2
Kassandra <kassandraiatrou@gmail.com>
2025-08-31 13:06:47 +1000

    task 9

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -42,5 +42,5 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-
+export { listOfStudents };
 export default listOfStudents;
