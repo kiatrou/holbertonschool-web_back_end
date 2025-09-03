9fc455e90c0a3c9a2086ebe75d3721da3710ca33
Kassandra <kassandraiatrou@gmail.com>
2025-09-03 16:46:22 +1000

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -7,7 +7,7 @@ export default function cleanSet(set, startString) {
     // converted set to array
     const arr = [...set];
     // filtered through array to find values that start with startString
-    const matching = arr.filter(value => value.startsWith(startString));
+    const matching = arr.filter(value => typeof value === 'string' && value.startsWith(startString));
     // removed the prefix from the values
     const withoutPrefix = matching.map(value => value.slice(startString.length));
     // joined the values with -
