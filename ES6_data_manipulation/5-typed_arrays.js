a4445bc5139cb681e7955e4469a73ceee2f0541b
Kassandra <kassandraiatrou@gmail.com>
2025-09-03 15:53:11 +1000

    task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+    const buffer = new ArrayBuffer(length);
+    const newArr = new Int8Array(buffer);
+
+    if (position < 0 || position >= length) {
+        throw new Error("Position outside range");
+    } else {
+        newArr[position] = value;
+        return buffer;
+    }
+}
