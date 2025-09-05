02cd9c1e628a624dc3525a47f0d81beee2db4675
Kassandra <kassandraiatrou@gmail.com>
2025-09-05 10:16:25 +1000

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,5 +1,5 @@
-import uploadPhoto from "utils.js"
-import createUser from "utils.js"
+import uploadPhoto from "./utils.js"
+import createUser from "./utils.js"
 
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
