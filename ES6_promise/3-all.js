2dae624bbbce35b119363cff3e5357c69b970070
Kassandra <kassandraiatrou@gmail.com>
2025-09-05 10:15:22 +1000

    task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+import uploadPhoto from "utils.js"
+import createUser from "utils.js"
+
+export default function handleProfileSignup() {
+    return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+        const body = results[0].body;
+        const firstName = results[1].firstName;
+        const lastName = results[1].lastName;
+
+        console.log(body, firstName, lastName);
+    })
+
+    .catch(() => {
+        console.log("Signup system offline")
+        return new Error();
+    })
+}
