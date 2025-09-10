e1dbdc32c8d796bf201cd3ef123d3ed4c41b9d75
Kassandra <kassandraiatrou@gmail.com>
2025-09-10 15:14:33 +1000

    task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+module.exports = function countStudents(filename) {
+  try {
+
+    const data = fs.readFileSync(filename, 'utf-8');
+    const rows = data.split('\n').slice(1);
+
+    const studentsCS = [];
+    const studentsSWE = [];
+
+    for (const row of rows) {
+      const data = row.split(',');
+
+      if (data[3] === 'CS') { // hardcode
+        studentsCS.push(data[0]);
+      }
+
+      if (data[3] === 'SWE') { // hardcode
+        studentsSWE.push(data[0]);
+      }
+    }
+
+    console.log(`Number of students: ${studentsCS.length + studentsSWE.length}`);
+    console.log(`Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}`);
+    console.log(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
