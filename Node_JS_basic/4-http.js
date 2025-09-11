da476d820ca21104d57124b2677ea04581484558
Kassandra <kassandraiatrou@gmail.com>
2025-09-11 10:41:03 +1000

    task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,19 +1,38 @@
+// const http = require('http');
+
+// // Every time someone visits your website (makes an HTTP request), the function (req, res) => { } gets called.
+// const app = http.createServer((req, res) => {
+//     // What this means: "I'm about to send a successful response containing plain text"
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+//     // What happens: The browser receives "Hello, World!" as the response.
+//     res.end('Hello, World!\n');
+// });
+
+// // define the PORT = Like a doorway on your computer where network traffic comes in
+// const PORT = 1245;
+
+// app.listen(PORT, 'localhost', () => {
+//   console.log("Hello Holberton School!");
+// });
+
+// module.exports = app;
+// ----- Old code that works ends here -----
+
+
 const http = require('http');
 
-// Every time someone visits your website (makes an HTTP request), the function (req, res) => { } gets called.
-const app = http.createServer((req, res) => {
-    // What this means: "I'm about to send a successful response containing plain text"
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+const host = 'localhost';
+const port = 1245;
 
-    // What happens: The browser receives "Hello, World!" as the response.
-    res.end('Hello, World!\n');
-});
+function requestListener(req, res) {
+  res.writeHead(200);
+  res.end('Hello Holberton School!');
+}
 
-// define the PORT = Like a doorway on your computer where network traffic comes in
-const PORT = 1245;
-
-app.listen(PORT, 'localhost', () => {
-  console.log("Hello Holberton School!");
+const app = http.createServer(requestListener);
+app.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
 });
 
 module.exports = app;
